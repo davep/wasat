b94cb9a7c47f3a85cba6b3ff8d0e737f487d2c5e
Dave Pearson <davep@davep.org>
2026-06-18 09:00:24 +0100

    :bulb: Add new class marker comments

--- src/wasat/exceptions.py
+++ src/wasat/exceptions.py
@@ -1,26 +1,32 @@
 """Exceptions for the Wasat Gemini client library."""
 
 
+##############################################################################
 class WasatError(Exception):
     """Base exception for all errors raised by the Wasat library."""
 
 
+##############################################################################
 class URIError(WasatError):
     """Raised when a Gemini URI is invalid or malformed."""
 
 
+##############################################################################
 class ProtocolError(WasatError):
     """Raised when the Gemini server violates the protocol."""
 
 
+##############################################################################
 class ConnectionError(WasatError):
     """Raised when a connection to the Gemini server fails."""
 
 
+##############################################################################
 class SecurityError(WasatError):
     """Raised when TLS or certificate validation (TOFU) fails."""
 
 
+##############################################################################
 class RedirectError(WasatError):
     """Raised when redirect limits are exceeded or redirects are invalid."""
 
